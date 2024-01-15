c3cf896b222153503a7749c4046d3411ce967662
KUMAR-TINKU <154230279+KUMAR-TINKU@users.noreply.github.com>
2024-01-15 20:07:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+// dynamically items generated using javascript
+
+displayItemsOnHomePage();
+function displayItemsOnHomePage(){
+  let itemsContainerElement=document.querySelector('.items-container');
+  let innerHTML='';
+    items.forEach(item=>{
+    innerHTML+= `
+    <div class="item-container">
+      <img class="item-image" src="${item.image}" alt="item images">
+      <div class="rating">
+    ${item.rating.stars}⭐ | ${item.rating.count}
+      </div>
+      <div class="company-name">${item.company_name}</div>
+      <div class="item-name">${item.item_name}</div>
+      <div class="price">
+        <span class="current-price">Rs ${item.current_price}</span>
+        <span class="original-price">Rs ${item.original_price}</span>
+        <span class="discount">(${item.discount_percentage}% OFF)</span>
+      </div>
+      <button class="btn-add-bag" onclick=addToBag(${item.id});>Add to Bag</button>
+    </div>`
+    });
+  
+  itemsContainerElement.innerHTML=innerHTML;
+}
+
